28c0ebcacc33214565edbab3352a66a049932cc0
gcsvieira <74613475+gcsvieira@users.noreply.github.com>
2023-02-25 00:03:03 -0300

    Conexão com o banco de dados

---
+++ src/application/App.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
